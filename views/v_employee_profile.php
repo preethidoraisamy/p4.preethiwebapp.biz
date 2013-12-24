2a36473a8ecaa5c9507b20424368d84e7e23059d
Preethi <preethi_rajasekar16@yahoo.com>
2013-12-23 19:06:20 -0800

    comments

--- views/v_employee_profile.php
+++ views/v_employee_profile.php
@@ -6,36 +6,36 @@
 			<legend>Edit profile</legend>
 
 				Full Name(40 characters)
-				<input type='text' name='fullname' maxlength="40"><br>
+				<input type='text' name='fullname' maxlength="40" required="required"><br>
 
 				Experience(2 characters)
-				<input type='text' name='experience' maxlength="2" id = "experience"><br>
+				<input type='text' name='experience' maxlength="2" id = "experience" required="required"><br>
 
 				Last company(40 characters)
-				<input type='text' name='last_company' maxlength="40"><br>
+				<input type='text' name='last_company' maxlength="40" required="required"><br>
 
 				Highest Degree(10 characters)
-				<input type='text' name='highest_degree' maxlength="10"><br>
+				<input type='text' name='highest_degree' maxlength="10" required="required"><br>
 
 				Major(20 characters)
-				<input type='text' name='major' maxlength="20"><br>
+				<input type='text' name='major' maxlength="20" required="required"><br>
 
-				Zipcode(6 characters)
-				<input type='text' name='zipcode' maxlength="20"><br>
+				Zipcode(5 characters)
+				<input type='text' name='zipcode' maxlength="5" id ="zipcode" required="required"><br>
 
 				<!-- To enter the Cv description -->
 				Description - About yourself(100 characters)
-				<input type='text' name='description' maxlength="40"><br>
+				<input type='text' name='description' maxlength="40" required="required"><br>
 
  				<!-- To enter the skills -->
 				Skills (255 characters)
-				<input type='text' name='skills' maxlength="255"><br>
+				<input type='text' name='skills' maxlength="255" required="required"><br>
 
 				Phone # (10 characters)
-				<input type='text' name='phone' maxlength="10" id = "phoneno"><br>
+				<input type='text' name='phone' maxlength="10" id = "phoneno" required="required"><br>
 
 				Prefered Email(50 charaters)
-				<input type='text' name='pemail' maxlength="10"><br>
+				<input type='text' name='pemail' maxlength="10" required="required"><br>
 	
 				<br><input type='Submit' value='Save Profile'>
 		
--- views/v_employer_profile.php
+++ views/v_employer_profile.php
@@ -7,36 +7,36 @@
 
 				<!-- To enter the Employer name  -->
 				Name (100 characters)
-				<input type='text' name='companyName' maxlength="100"><br>
+				<input type='text' name='companyName' maxlength="100" required="required"><br>
 
 				<!-- To enter the Employer Description  -->
 				Description (250 characters)
 				<!-- <input type='text' name='description' maxlength="250"><br> -->
-				<textarea name='description' maxlength="250"></textarea><br>
+				<textarea name='description' maxlength="250" required="required"></textarea><br>
 
 
 				<!-- To enter the Employer Revenue  -->
 				Revenue (250 characters)
 				<!-- <input type='text' name='revenue' maxlength="250"><br> -->
-				<textarea name='revenue' maxlength="250"></textarea><br>
+				<textarea name='revenue' maxlength="250" required="required"></textarea><br>
 
 				<!-- To enter the Employer Industry  -->
 				Industry (250 characters)
 				<!-- <input type='text' name='industry' maxlength="250"><br> -->
-				<textarea name='industry' maxlength="250"></textarea><br>
+				<textarea name='industry' maxlength="250" required="required"></textarea><br>
 
 				<!-- To enter the Employer Address  -->
 				Street Name and Building # (60 characters)
-				<input type='text' name='street_name' maxlength="60"><br>
+				<input type='text' name='street_name' maxlength="60" required="required"><br>
 
-				City (20 characters)
-				<input type='text' name='city' maxlength="20"><br>
+				City (20 characters) 
+				<input type='text' name='city' maxlength="20" required="required"><br>
 
 				State (2 characters)
-				<input type='text' name='state' maxlength="2"><br>
+				<input type='text' name='state' maxlength="2" required="required"><br>
 
- 				Zipcode(6 characters)
-				<input type='text' name='zipcode' maxlength="6"><br>
+ 				Zipcode(5 characters)
+				<input type='text' name='zipcode' maxlength="5" id ="zipcode" required="required"><br>
 	
 				<br><input type='Submit' value='Save Profile'>
 		
